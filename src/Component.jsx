33b9df249e8657e45c2cd1c044941d79f8667c5e
SMarulanda19 <s.quintero@globant.com>
2024-02-26 15:03:02 -0500

    First Commit

---
+++ src/Component.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+export default function Component() {
+ 
+    const [ text, setText] = useState()
+    const [ update, setUpdate] = useState()
+
+     const textOnchage = (event) => {
+        setText(event.target.value)
+     }
+
+     const buttonOnClick = () => {
+        setUpdate(text)
+     }
+
+
+
+    return (
+    <div>
+
+        <input type="text" value={text} onChange={textOnchage}/>
+        <button onClick={buttonOnClick}> Actualizar</button>
+        <p>Texto input: {text}</p>
+        <p>texto Actualizado: {update}</p>
+
+
+    </div>
+  )
+}
